6707d1fc3ed714e839e41b0494b638ad396f61cf
Efrat Weitzman <64781412+Efrat-Weitzman@users.noreply.github.com>
2021-08-29 17:21:15 +0300

    Update App.js
    add request to api and render the TOC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import TableOfContent from './components/TableOfContent'
 import './App.css';
+import Film from './components/Film';
 
-function App() {
+const App = () => {
+  const [films, setFilms] = useState([]);
+  const [chosenFilm,setChosenFilm]=useState(null);
+  const [isChosen,setIsChosen]=useState(false);
+  const getFilmsRequest = async () => {
+    const url = "https://www.swapi.tech/api/films/";
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    if (responseJson.result) {
+      setFilms(responseJson.result);
+    }
+  };
+
+  useEffect(() => {
+    getFilmsRequest();
+  }, []);
+
+  function afterChooseFile(currentFilm){
+    setChosenFilm(currentFilm);
+    setIsChosen(true);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid movie-app'>
+      <div className='row'>
+        <TableOfContent
+          films={films}
+          handleChooseFilm={afterChooseFile}
+        />
+      </div>
+      {/* <button onClick={()=>afterChooseFile(films[2])}>try</button> */}
+      {isChosen && (
+            <Film chosenFilm={chosenFilm}/>)}
     </div>
   );
-}
+};
 
 export default App;
