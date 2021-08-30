989db0ed8705038f471c91bb2031867c8c624585
Efrat Weitzman <64781412+Efrat-Weitzman@users.noreply.github.com>
2021-08-30 07:47:17 +0300

    Update TableOfContent.js
    add elements and classes for css

--- src/components/TableOfContent.js
+++ src/components/TableOfContent.js
@@ -2,17 +2,21 @@ import React from 'react';
 
 const TableOfContent = (props) => {
     return (
-        <>
-            {props.films.map((film, index) => (
-                <div key={film.uid}
-                    className='image-container d-flex justify-content-start m-3'
-                >
-                    <div onClick={() => props.handleChooseFilm(film)} title={'click to view details'}>
-                        <b>{film.properties.title}</b>
+        <div className="articles card">
+            <div className="card-header d-flex align-items-center">
+                <h3 className="card-title align-items-start flex-column">Films List</h3>
+            </div>
+            <div className="card-body pt-6">
+                {props.films.map((film) => (
+                    <div key={film.uid} className='item'>
+                        <div onClick={() => props.handleChooseFilm(film)}
+                            title={'click to view details'}>
+                            <b>{film.properties.title}</b>
+                        </div>
                     </div>
-                </div>
-            ))}
-        </>
+                ))}
+            </div>
+        </div>
     );
 };
 export default TableOfContent;
