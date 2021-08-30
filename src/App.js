769924b46a6fc048ad952a71ddc2c68e9165d400
Efrat Weitzman <64781412+Efrat-Weitzman@users.noreply.github.com>
2021-08-30 07:51:31 +0300

    Update App.js
    add elements and classes for css and rearrange the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import TableOfContent from './components/TableOfContent'
-import './App.css';
+import TableOfContent from './components/TableOfContent';
 import Film from './components/Film';
+import './App.css';
 
 const App = () => {
   const [films, setFilms] = useState([]);
-  const [chosenFilm,setChosenFilm]=useState(null);
-  const [isChosen,setIsChosen]=useState(false);
+  const [chosenFilm, setChosenFilm] = useState(null);
+  const [isChosen, setIsChosen] = useState(false);
   const [favoritesList, setFavoritesList] = useState([]);
 
   const getFilmsRequest = async () => {
@@ -23,40 +23,47 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-		const filmsFavorites = JSON.parse(
-			localStorage.getItem('star-wars-app-favorites')
-		);
-		if (filmsFavorites) {
-			setFavoritesList(filmsFavorites);
-		}
-	}, []);
+    const filmsFavorites = JSON.parse(
+      localStorage.getItem('star-wars-app-favorites')
+    );
+    if (filmsFavorites) {
+      setFavoritesList(filmsFavorites);
+    }
+  }, []);
 
-  function afterChooseFilm(currentFilm){
+  function afterChooseFilm(currentFilm) {
     setChosenFilm(currentFilm);
     setIsChosen(true);
   }
 
-  function addToFavoriteBtn(filmId){
+  function addToFavoriteBtn(filmId) {
     const newFavoritesList = [...favoritesList, filmId];
-		setFavoritesList(newFavoritesList);
-		saveToLocalStorage(newFavoritesList);
+    setFavoritesList(newFavoritesList);
+    saveToLocalStorage(newFavoritesList);
   }
 
   const saveToLocalStorage = (items) => {
-		localStorage.setItem('star-wars-app-favorites', JSON.stringify(items));
-	};
+    localStorage.setItem('star-wars-app-favorites', JSON.stringify(items));
+  };
 
   return (
-    <div className='container-fluid movie-app'>
-      <div className='row'>
-        <TableOfContent
-          films={films}
-          handleChooseFilm={afterChooseFilm}
-        />
+    <div className="d-flex flex-column-fluid">
+      <div className='container'>
+        <div className='row'>
+          <div className="col-lg-4">
+            <TableOfContent
+              films={films}
+              handleChooseFilm={afterChooseFilm}
+            />
+          </div>
+          {isChosen && (
+            <div className="col-lg-8">
+              <Film chosenFilm={chosenFilm} handleFavoriteBtn={addToFavoriteBtn} listFavorites={favoritesList} />
+            </div>)}
+        </div>
+        <div>
+        </div>
       </div>
-
-      {isChosen && (
-            <Film chosenFilm={chosenFilm}  handleFavoriteBtn={addToFavoriteBtn} listFavorites={favoritesList}/>)}
     </div>
   );
 };
